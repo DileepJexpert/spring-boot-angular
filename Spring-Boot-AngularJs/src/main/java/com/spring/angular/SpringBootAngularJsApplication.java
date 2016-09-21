0c5f8a301ec2565e55debdb77c9c8b39bff7f065
DileepkMaurya <todileepmaurya@gmail.com>
2016-09-21 13:38:46 +0400

    First commit

---
+++ Spring-Boot-AngularJs/src/main/java/com/spring/angular/SpringBootAngularJsApplication.java
@@ -0,0 +1,16 @@
+package com.spring.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularJsApplication.class, args);
+		//SpringApplication app = new SpringApplication(SpringBootAngularJsApplication.class);
+		//app.setBannerMode(Banner.Mode.OFF);
+	   // app.run(args);
+	}
+}
